549d8d5e61dd74269cd0f0f2315e31e8ac904ef2
Jacob Zastrow <jake.zastrow@kellogg.northwestern.edu>
2026-02-09 23:46:19 +0000

    updated destroy model code

--- kmdb.rb
+++ kmdb.rb
@@ -78,7 +78,11 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
-Model.destroy_all
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
+Agent.destroy_all
 
 # Generate models and tables, according to the domain model.
 # TODO!
